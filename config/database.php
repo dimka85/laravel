0851ac5641827259816ac51e43737cbe97664ed5
Dmitry <tsibdmitry85@gmail.com>
2018-06-10 00:24:03 +0300

    Install all php and js broadcasting dependencies and consigure basic settings

--- config/database.php
+++ config/database.php
@@ -149,6 +149,13 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => 5,
         ],
+    
+        'broadcast' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 6,
+        ],
 
     ],
 
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -46,6 +46,13 @@ if (token) {
 
 // import Echo from 'laravel-echo'
 
+// window.io = require('socket.io-client');
+
+// window.Echo = new Echo({
+//     broadcaster: 'socket.io',
+//     host: window.location.hostname + ':6001'
+// });
+
 // window.Pusher = require('pusher-js');
 
 // window.Echo = new Echo({
