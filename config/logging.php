986e6a3f324c0aa42f8622ddf7af14b112d4472a
Dmitry <tsibdmitry85@gmail.com>
2018-06-09 19:16:37 +0300

    Configure daily logging

--- config/logging.php
+++ config/logging.php
@@ -48,7 +48,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
-            'days' => 7,
+            'days' => env('LOG_DAILY_DAYS', 7),
         ],
 
         'slack' => [
