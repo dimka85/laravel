0b8a7c4b99b99aa964b32053068c7bd27915f67f
Dmitry <tsibdmitry85@gmail.com>
2018-06-09 18:30:57 +0300

    Configure sessions and redis db for sessions

--- config/database.php
+++ config/database.php
@@ -121,6 +121,13 @@ return [
             'port' => env('REDIS_PORT', 6379),
             'database' => 1,
         ],
+    
+        'session' => [
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 2,
+        ],
 
     ],
 
