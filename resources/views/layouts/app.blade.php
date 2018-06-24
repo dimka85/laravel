3d9a520f8c9ef1b961cfa84c65f869359992028c
Dmitry <tsibdmitry85@gmail.com>
2018-06-24 11:05:24 +0300

    Fix some basic views

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,17 +8,21 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', config('app.name'))</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    @section('scripts')
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    @show
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @section('styles')
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    @show
 </head>
 <body>
     <div id="app">
