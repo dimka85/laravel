91932827760faf735fffb46ceb0899efb9096483
Dmitry <tsibdmitry85@gmail.com>
2018-06-25 14:40:18 +0300

    Rework login file forms and links to laravelcollective

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,14 +8,15 @@
                 <div class="card-header">{{ __('Login') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                    {{ Form::open(['route' => 'login']) }}
 
                         <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            {{ Form::label('email', __('E-Mail address'), ['class' => 'col-sm-4 col-form-label
+                            text-md-right']) }}
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                {{ Form::email('email', old('email'), ['class' => 'form-control' . ($errors->has
+                                ('email') ? ' is-invalid' : ''), 'required', 'autofocus']) }}
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback">
@@ -26,10 +27,10 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            {{ Form::label('password', __('Password'), ['class' => 'col-md-4 col-form-label text-md-right']) }}
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                {{ Form::password('password', ['class' => 'form-control' . ($errors->has('password') ? ' is-invalid' : ''), 'required']) }}
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
@@ -43,7 +44,8 @@
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> {{ __('Remember Me') }}
+                                        {{ Form::checkbox('remember', null, (old('remember') ? true : false)) }} {{
+                                        __('Remember me') }}
                                     </label>
                                 </div>
                             </div>
@@ -51,16 +53,13 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                                {{ Form::submit(__('Login'), ['class' => 'btn btn-primary']) }}
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
+                                {{ Html::linkRoute('password.request', __('Forgot your password?'), [], ['class' => 'btn btn-link']) }}
                             </div>
                         </div>
-                    </form>
+
+                    {{ Form::close() }}
                 </div>
             </div>
         </div>
