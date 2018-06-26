b2d3344e48fd79604e85a7e2aecfd9e887177e79
Dmitry <tsibdmitry85@gmail.com>
2018-06-26 08:38:26 +0300

    Rework register file forms and links to laravelcollective

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,14 +8,15 @@
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+                    {{ Form::open(['route' => 'register']) }}
 
                         <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                            {{ Form::label('name', __('Name'), ['class' => 'col-md-4 col-form-label
+                            text-md-right']) }}
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                {{ Form::text('name', old('name'), ['class' => 'form-control' . ($errors->has
+                                ('name') ? ' is-invalid' : ''), 'required', 'autofocus']) }}
 
                                 @if ($errors->has('name'))
                                     <span class="invalid-feedback">
@@ -26,10 +27,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            {{ Form::label('email', __('E-Mail address'), ['class' => 'col-md-4 col-form-label
+                            text-md-right']) }}
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                {{ Form::email('email', old('email'), ['class' => 'form-control' . ($errors->has
+                                ('email') ? ' is-invalid' : ''), 'required']) }}
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback">
@@ -40,10 +43,12 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            {{ Form::label('password', __('Password'), ['class' => 'col-md-4 col-form-label
+                            text-md-right']) }}
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                {{ Form::password('password', ['class' => 'form-control' . ($errors->has('password')
+                                ? ' is-invalid' : ''), 'required']) }}
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
@@ -54,21 +59,27 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            {{ Form::label('password-confirm', __('Confirm password'), ['class' => 'col-md-4
+                            col-form-label text-md-right']) }}
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                                {{ Form::password('password-confirm', ['class' => 'form-control', 'required']) }}
+
+                                @if ($errors->has('password-confirm'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('password-confirm') }}</strong>
+                                    </span>
+                                @endif
                             </div>
                         </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
+                                {{ Form::submit(__('Register'), ['class' => 'btn btn-primary']) }}
                             </div>
                         </div>
-                    </form>
+
+                    {{ Form::close() }}
                 </div>
             </div>
         </div>
